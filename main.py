03b1b3192b8c1d879367ec8737fb00b079b30c80
Srijan Sarkar <srijanarkar.coding@gmail.com>
2026-02-20 23:05:11 +0530

    Added cause and prevention for more details

--- main.py
+++ main.py
@@ -14,23 +14,23 @@ model = model_from_json(model_json)
 model.load_weights("emotion.h5")
 
 disease_labels = [
-    "Bacterial Blight",
-    "Blast",
-    "Brown Spot",
-    "Tungro"
-]
+    ["Bacterial Blight", "Spreads via rain, irrigation water, wind", "Avoid excessive nitrogen fertilizers"],
+    ["Blast", "Spreads through spores in air", "Proper plant spacing"],
+    ["Brown Spot", "Drought stress", "Use certified disease-free seeds"],
+    ["Tungro", "Virus transmitted by insect vectors", "Use resistant rice varieties"]]
+
 
 @app.get("/")
 def Rice():
-    return {"Project" : "Anay sir's rice disease prediction"}
+    return {"Project": "Anay sir's rice disease prediction"}
+
 
 @app.get("/health")
 def status():
-    return {"status" : "Up and running"}
+    return {"status": "Up and running"}
 
 
 def imageTransform(content: bytes):
-
     # Open image using PIL
     image = Image.open(io.BytesIO(content))
 
@@ -51,22 +51,25 @@ def imageTransform(content: bytes):
 
     return img_array
 
+
 @app.post("/predict")
 async def predictRiceDisease(file: UploadFile = File(...)):
     # Read image as bytes
     contents = await file.read()
 
-    # Getting back pre-processed image
+    # Getting back pre-processed image in form of array
     img_array = imageTransform(contents)
 
-# Prediction
+    # Prediction
     pred = model.predict(img_array)[0]
     predicted_index = np.argmax(pred)
 
-    disease_tag = disease_labels[predicted_index]
-    confidence = int(pred[predicted_index]*100)
+    disease_tag = disease_labels[predicted_index][0]
+    confidence = int(pred[predicted_index] * 100)
 
     return {
         "disease": disease_tag,
-        "confidence": confidence
-}
+        "confidence": confidence,
+        "cause": disease_labels[predicted_index][1],
+        "prevention": disease_labels[predicted_index][2],
+    }
